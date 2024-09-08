38cf2f81792f40f50dc88e9a87489b5c1649c223
Mission Gaire <163767603+MissionbyTech@users.noreply.github.com>
2024-09-09 02:24:19 +0545

    Add files via upload

---
+++ Mission ;)Java Script/hello/mission.js
@@ -0,0 +1,10 @@
+const profile =
+{
+    username :"Mission Gaire",
+    posts : 195,
+    followers: 124,
+    following : 5,
+    isFollow: false,
+};
+<script src="https://kit.fontawesome.com/51d88a67ad.js" crossorigin="anonymous"></script>
+console.log(typeof profile.posts);
